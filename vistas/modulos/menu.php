28dd80bcfbf6943177ebc659668306c7488a2782
PatoTaactico <ycamigo123@gmail.com>
2025-03-17 19:15:24 -0500

    algo

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -11,10 +11,10 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="vistas/dist/img/usuarios/usuario1.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="vistas/dist/img/usuarios/usuario2.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Kazad Ebrocoli</a>
+                <a href="#" class="d-block">Elza Pato</a>
             </div>
         </div>
 
